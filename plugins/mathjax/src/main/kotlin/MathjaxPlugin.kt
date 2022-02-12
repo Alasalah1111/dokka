4524224571b1270c9acad0d8dee53f0b07949c6b
vmishenev <vad-mishenev@yandex.ru>
2022-02-12 20:08:58 +0300

    Make documentables in page interface

--- plugins/mathjax/src/main/kotlin/MathjaxPlugin.kt
+++ plugins/mathjax/src/main/kotlin/MathjaxPlugin.kt
@@ -5,6 +5,7 @@ import org.jetbrains.dokka.CoreExtensions
 import org.jetbrains.dokka.model.doc.CustomTagWrapper
 import org.jetbrains.dokka.pages.ContentPage
 import org.jetbrains.dokka.pages.RootPageNode
+import org.jetbrains.dokka.pages.WithDocumentables
 import org.jetbrains.dokka.plugability.DokkaPlugin
 import org.jetbrains.dokka.transformers.pages.PageTransformer
 
@@ -25,8 +26,7 @@ object MathjaxTransformer : PageTransformer {
     }
 
     private val ContentPage.isNeedingMathjax
-        get() = documentable?.documentation?.values
-            ?.flatMap { it.children }
-            .orEmpty()
-            .any { (it as? CustomTagWrapper)?.name == ANNOTATION }
+        get() = (this as WithDocumentables).documentables.any { it.documentation.values
+            .flatMap { it.children }
+            .any { (it as? CustomTagWrapper)?.name == ANNOTATION } }
 }
